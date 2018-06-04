75b3755769edf2847119646babcd408f6303f06b
Eugene Mitskevich <evg.mitskevich@gmail.com>
2018-06-04 18:20:39 +0300

    Import fix

--- adfor/settings.py
+++ adfor/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
-import os
+import os, sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 from django.urls import reverse
