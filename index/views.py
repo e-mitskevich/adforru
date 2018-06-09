d66cab31f10c029cddcbe66c9551d035b4f0c575
Eugene Mitskevich <evg.mitskevich@gmail.com>
2018-06-09 23:33:35 +0300

    Ebay endpoint

--- index/views.py
+++ index/views.py
@@ -1,10 +1,14 @@
 import json
 import uuid
+from urllib.parse import urlparse, parse_qs
 
+import requests
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.utils.http import urlencode
+
 from index.models import TrackerLogEntry
 
 
@@ -35,3 +39,23 @@ def tracker(request):
     mark = request.COOKIES.get(COOKIE_NAME, str(uuid.uuid1()))
     response.set_cookie(COOKIE_NAME, mark, domain=request.META.get("HTTP_HOST", "").split(":")[0])
     return response
+
+
+def ebay(request):
+    get_params = {
+        'SECURITY-APPNAME': 'EvgenijM-Testappl-PRD-62ccbdeee-ae1b5bd7',
+        'OPERATION-NAME': 'findItemsByKeywords',
+        'SERVICE-VERSION': '1.0.0',
+        'RESPONSE-DATA-FORMAT': 'JSON',
+        'callback': '_cb_findItemsByKeywords',
+        'keywords': 'iPhone 7 plus 32gb unlocked black',
+        'paginationInput.entriesPerPage': '100',
+        'sortOrder': 'PricePlusShippingLowest',
+        'GLOBAL-ID': 'EBAY-US',
+        'siteid': '0'
+    }
+
+    url = "https://svcs.ebay.com/services/search/FindingService/v1?" + urlencode(get_params)
+    result = requests.get(url).text
+
+    return render(request, "index/ebay.html", locals())
